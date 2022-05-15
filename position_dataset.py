1ee2040c0e031c80f1d6e49ba7c20ad387254d9d
Reuven <reuvenpe2005@gmail.com>
2022-05-15 22:39:38 +0300

    Prepare tensor encoding for positions

--- position_dataset.py
+++ position_dataset.py
@@ -2,15 +2,36 @@
 Define an iterable dataset for chess position
 """
 from torch.utils.data import IterableDataset
+import torch.nn.functional as F
 import chess.pgn
+import chess
 
+
+def position_to_tensor(position: chess.Board):
+    """
+    Encode a position as a 8 * 8 * 12 tensor (heigth * width * colot * piecetype)
+    piecetype encoding is pnbrqkPNBRQK. tensor index [0, 0] is A1, [0, 7] is H1.
+    """
+    def encode_piece(piece):
+        return piece.color * 6 + piece.piece_type - 1
+    res = torch.zeros(64, 12)
+    for sq in chess.SQUARES:
+        p = position.piece_at(sq)
+        if p is None:
+            continue
+        res[sq, encode_piece(p)] = 1
+    return torch.reshape(res, (8, 8, 12))
 
 class PositionDataset(IterableDataset):
     def _game_to_position_iter(self, game: chess.pgn.Game):
         board = game.board()
         for move in game.mainline_moves():
             board.push(move)
-            yield board
+            yield {
+                'game': game,
+                'position': board,
+                'last_move': move
+            }
 
     def __init__(self, pgn_file):
         self.pgn_file = pgn_file
@@ -22,3 +43,11 @@ class PositionDataset(IterableDataset):
             for position in self._game_to_position_iter(game):
                 yield position
             game = chess.pgn.read_game(self.pgn_file)
+
+
+class LastMoveDataset(IterableDataset):
+    def __init__(self, pgn_file):
+        self.position_dataset = PositionDataset(pgn_file)
+
+    def __iter__(self):
+        pass
