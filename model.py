37d9ab979daafe36b8b8a7380c0e195f16197123
Reuven <reuvenpe2005@gmail.com>
2022-05-22 22:27:50 +0300

    Add convolution layer

--- model.py
+++ model.py
@@ -8,8 +8,10 @@ class LastMovePredictor(pl.LightningModule):
     def __init__(self):
         super().__init__()
         self.predict = nn.Sequential(
+            nn.Conv2d(12, 64, 3, padding='same'),
+            nn.Conv2d(64, 64, 3, padding='same'),
             nn.Flatten(),
-            nn.Linear(64 * 12, 512),
+            nn.Linear(64 * 64, 512),
             nn.ReLU(),
             nn.Linear(512, 64),
             nn.Softmax(0)
@@ -25,14 +27,14 @@ class LastMovePredictor(pl.LightningModule):
 
     def training_step(self, train_batch, batch_idx):
         x, y = train_batch
-        x = x.view(x.size(0), -1)
+        x = x.permute((0, 3, 1, 2))
         pred = self.predict(x)
         loss = F.cross_entropy(pred, y)
         return loss
 
     def validation_step(self, val_batch, batch_idx):
         x, y = val_batch
-        x = x.view(x.size(0), -1)
+        x = x.permute((0, 3, 1, 2))
         pred = self.predict(x)
         loss = F.cross_entropy(pred, y)
         return loss
